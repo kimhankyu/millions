d8055d60531e43b68963e95aab31e76726b1c38b
hkkim <hkkim0103@naver.com>
2019-07-31 21:08:23 +0900

    fucking heroku

--- millions/millions/wsgi.py
+++ millions/millions/wsgi.py
@@ -11,5 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ["DJANGO_SETTINGS_MODULE"] = "millions.settings"
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'millions.settings')
 application = get_wsgi_application()
